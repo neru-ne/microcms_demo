99d1308fd1bdef2fc741085e7fa692dc549a4255
neru-ne <b_scorpion@me.com>
2024-04-08 23:05:02 +0900

    [update]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { MainContents } from '@/app/components/layouts/MainContents'
-import {Head} from '@/app/components/layouts/Head'
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
-      <Head/>
       <MainContents>
         HOME
         <ul>
