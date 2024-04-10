169a5d3a9e82d57de3bd62e6c8c5bbc3cd9a1b89
Mika Ueda <ueda.m@nadia.bz>
2024-04-10 16:02:56 +0900

    [update] buildでこけたところの修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 "use client"
 import "./globals.css";
 
+import { Suspense } from "react";
 import { RecoilRoot } from 'recoil';
 import { SetOption } from '@/app/components/atoms/options/SetOption'
 import { Head } from '@/app/components/layouts/Head'
@@ -17,8 +18,10 @@ export default function RootLayout({
       <RecoilRoot>
         <Head />
         <body >
+          <Suspense>
             <SetOption />
             {children}
+          </Suspense>
         </body>
       </RecoilRoot>
     </html>
